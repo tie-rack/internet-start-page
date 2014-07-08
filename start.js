102c42a8c5351335cc155255e288eb345986e1f1
Chris Shea <cmshea@gmail.com>
2014-07-08 12:47:07 -0400

    Google news search for Firefox

--- start.js
+++ start.js
@@ -87,6 +87,8 @@ var menu = [
     links: [
       { href: "http://www.reddit.com/r/FirefoxInfo/",
         text: "/r/FirefoxInfo" },
+      { href: "https://www.google.com/search?q=firefox#q=firefox&tbm=nws",
+        text: "google news search" },
       { rss: "https://blog.mozilla.org/feed/",
         url: "https://blog.mozilla.org/",
         text: "mozilla blog" },
